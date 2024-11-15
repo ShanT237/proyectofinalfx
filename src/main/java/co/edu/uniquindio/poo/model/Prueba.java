2d086b982838530683bc74c3a060827da98772a2
calculador <oscarm.morenop@uqvirtual.edu.co>
2024-11-14 23:41:47 -0500

    p2

---
+++ src/main/java/co/edu/uniquindio/poo/model/Prueba.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.poo.model;
+
+public class Prueba {
+    
+    private String nombre;
+    private int edad;
+
+    public Prueba(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+}
